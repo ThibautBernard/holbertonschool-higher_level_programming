6ce345199365268cec55b6c9b2016afbda87bffe
Thibaut <thibautbernard@sfr.fr>
2020-12-07 11:55:34 +0000

    feat(check_cycle)

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if there is a loop
+ * @list: the linked list
+ * Return: 0 if not, 1 if there is a loop
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	slow = list;
+	fast = list;
+	while (slow && fast)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}

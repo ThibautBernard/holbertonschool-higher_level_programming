1d9223b35fa69616ec6e47361bf4a53bc26e7b56
Thibaut <thibautbernard@sfr.fr>
2020-12-07 12:03:46 +0000

    efficiency

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,7 @@ int check_cycle(listint_t *list)
 
 	slow = list;
 	fast = list;
-	while (fast->next->next)
+	while (slow && fast && fast->next->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;

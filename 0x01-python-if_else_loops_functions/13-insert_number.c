7fbec11747e87655144943420228b23f0f87c793
Thibaut <thibautbernard@sfr.fr>
2020-12-08 12:16:06 +0000

    feat(): insert node

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - insert a new node in order sorted
+ * @head: adress of the linked list
+ * @number: number to add
+ * Return: null or adress of the new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *actual, *next, *new_node;
+
+	actual = *head;
+	next = actual->next;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	while (actual)
+	{
+		if (number < next->n)
+		{
+			new_node->next = actual->next;
+			actual->next = new_node;
+			new_node->n = number;
+			return (new_node);
+		}
+		else if (next->next == NULL)
+		{
+			new_node->next = next->next;
+			next->next = new_node;
+			new_node->n = number;
+			return (new_node);
+		}
+		actual = actual->next;
+		next = next->next;
+	}
+	return (new_node);
+}

1ac61998341ba2acc53fb9b98348ae447e92f8ff
Thibaut <thibautbernard@sfr.fr>
2020-12-08 11:32:28 +0000

    fix(): pep8 compliant

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for x in range(0, 10):
     for i in range(0, 10):
         if i > x:
-            print("{:d}".format(x), end="");
+            print("{:d}".format(x), end="")
             if x + i != 17:
-                print("{:d}".format(i), end=", ");
+                print("{:d}".format(i), end=", ")
             else:
-                print("{:d}".format(i));
+                print("{:d}".format(i))

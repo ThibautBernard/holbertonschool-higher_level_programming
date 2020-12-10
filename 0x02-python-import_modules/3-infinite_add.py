807cf6e6e341a42fee6c9c02d72fd2ce084cde48
Thibaut <thibautbernard@sfr.fr>
2020-12-10 11:17:51 +0000

    fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-nb_arg = len(sys.argv)
-counter = 0
-sum = 0
-nb_arg -= 1
-for i in sys.argv:
-    if counter > 0:
-        sum += int(i)
-    counter += 1
-print("{}".format(sum))
+if __name__ == "__main__":
+    import sys
+    nb_arg = len(sys.argv)
+    counter = 0
+    sum = 0
+    nb_arg -= 1
+    for i in sys.argv:
+        if counter > 0:
+            sum += int(i)
+        counter += 1
+    print("{}".format(sum))

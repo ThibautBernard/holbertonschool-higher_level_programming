e3ddf6617dab6598cce6624aaf919165c5ab232e
Thibaut <thibautbernard@sfr.fr>
2020-12-10 11:00:34 +0000

    feat()

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from calculator_1 import *
+import sys
+nb_arg = len(sys.argv)
+counter = 0
+sum = 0
+nb_arg -= 1
+if nb_arg != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+first = int(sys.argv[1])
+operator = sys.argv[2]
+second = int(sys.argv[3])
+if operator == "+":
+    print("{} + {} = {}".format(first, second, add(first, second)))
+elif operator == "-":
+    print("{} - {} = {}".format(first, second, sub(first, second)))
+elif operator == "/":
+    print("{} / {} = {}".format(first, second, div(first, second)))
+elif operator == "*":
+    print("{} * {} = {}".format(first, second, mul(first, second)))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)

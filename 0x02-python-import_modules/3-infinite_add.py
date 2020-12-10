b53d64688b0c427ef62a6d8e0ec2f456b096561b
Thibaut <thibautbernard@sfr.fr>
2020-12-10 10:06:37 +0000

    feat(infinite_add)

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+nb_arg = len(sys.argv)
+counter = 0
+sum = 0
+nb_arg -= 1
+for i in sys.argv:
+    if counter > 0:
+        sum += int(i)
+    counter += 1
+print("{}".format(sum))

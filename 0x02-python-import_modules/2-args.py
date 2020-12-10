bd5deb95471e793ec59eaa4fec490099790a8c80
Thibaut <thibautbernard@sfr.fr>
2020-12-10 09:58:37 +0000

    feat(args)

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+nb_arg = len(sys.argv)
+if nb_arg == 1:
+    print("0 arguments.")
+elif nb_arg == 2:
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+else:
+    counter = 0
+    nb_arg -= 1
+    print("{} arguments: ".format(nb_arg))
+    for i in sys.argv:
+        if counter > 0:
+            print("{}: {}".format(counter, i))
+        counter += 1

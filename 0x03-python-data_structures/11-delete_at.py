a444160a92385cb9f6599bdaebe87590f87a8df4
Thibaut <thibautbernard@sfr.fr>
2020-12-15 14:41:20 +0000

    feat()

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0:
+        return my_list
+    new_list = []
+    counter = 0
+    numb = 0
+    for i in my_list:
+        if idx == counter:
+            numb = i
+        counter += 1
+    my_list.remove(numb)
+    return my_list

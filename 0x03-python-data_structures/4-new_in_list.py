9a39a1352d80917bd1dbeb7d075b31de430cd8b3
Thibaut <thibautbernard@sfr.fr>
2020-12-15 11:44:02 +0000

    feat()

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    cpy_list = my_list[:]
+    if idx < 0:
+        return cpy_list
+    counter = 0
+    for i in cpy_list:
+        if counter == idx:
+            cpy_list[counter] = element
+            return cpy_list
+        counter += 1

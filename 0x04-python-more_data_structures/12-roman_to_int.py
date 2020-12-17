b35c020996778fc0afa214b26fcb16bf74ab7163
Thibaut <thibautbernard@sfr.fr>
2020-12-17 21:10:45 +0000

    feat()

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dico = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    sum = 0
+    last = 0
+    count = 0
+    for i in range(len(roman_string)):
+        for r, v in dico.items():
+            if roman_string[i] == r:
+                sum += v
+                if count == 0:
+                    last = v
+                    count += 1
+                if last < v:
+                    sum -= 2
+                if count != 0:
+                    last = v
+    return sum

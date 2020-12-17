330fe5b16bb5114ba887de086ddd012ef9e4772d
Thibaut <thibautbernard@sfr.fr>
2020-12-17 13:40:55 +0000

    feat()

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    count = 0
+    for k, v in a_dictionary.items():
+        if k == key:
+            a_dictionary[k] = value
+            count += 1
+    if count == 0:
+        a_dictionary[key] = value

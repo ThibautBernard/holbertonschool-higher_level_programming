19c3569eb62ba37ebe62a1725475556160da5dd3
Thibaut <thibautbernard@sfr.fr>
2020-12-17 11:29:15 +0000

    feat()

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    arr = []
+    occ = 0
+    sum = 0
+    arr.append(my_list[0])
+    for i in my_list:
+        occ = 0
+        for y in arr:
+            if i == y:
+                occ += 1
+                break
+        if occ == 0:
+            arr.append(i)
+    for z in arr:
+        sum += z
+    return sum

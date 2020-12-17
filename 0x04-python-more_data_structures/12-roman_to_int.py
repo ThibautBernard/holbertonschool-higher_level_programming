2455c3dca6c163aae520b25bd1fb3e4202c55ab1
Thibaut <thibautbernard@sfr.fr>
2020-12-17 21:18:47 +0000

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
     sum = 0
     last = 0
     count = 0
-    if roman_string is None or type(roman_string) != str:
+    if roman_string is None or type(roman_string) is not str:
         return 0
     for i in range(len(roman_string)):
         for r, v in dico.items():

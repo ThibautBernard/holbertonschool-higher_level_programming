0f9cc78f4f8b42d53a173ce352ddde476ed57511
Thibaut <thibautbernard@sfr.fr>
2021-01-04 11:36:10 +0000

    fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("division by 0")
             except TypeError:
                 new_tab.append(0)
-                print("Type")
+                print("wrong type")
             except IndexError:
                 new_tab.append(0)
                 print("out of range")

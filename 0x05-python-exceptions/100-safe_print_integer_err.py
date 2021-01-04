a52251bae2355ec56e264ad2460a246b9d85d19a
Thibaut <thibautbernard@sfr.fr>
2021-01-04 11:52:47 +0000

    fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,6 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        if (value <= 0 or value >= 0):
             print("{:d}".format(value))
             return True
     except Exception as n:

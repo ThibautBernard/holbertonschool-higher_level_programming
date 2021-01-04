bd0ffbfa365d2997ce1b07142a8d987ec4f88fb7
Thibaut <thibautbernard@sfr.fr>
2021-01-04 11:59:16 +0000

    feat()

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-            return fct(args[0], args[1])
+        return fct(args[0], args[1])
     except Exception as n:
         print("Exception: {}".format(n), file=sys.stderr)
         return None

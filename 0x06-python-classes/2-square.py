02fa19911fc013a5129776941ab753cfbb2df68d
Thibaut <thibautbernard@sfr.fr>
2021-01-05 09:24:34 +0000

    feat()

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.set_size(size)
+
+    def set_size(self, s):
+        try:
+            if (s < 0):
+                raise ValueError
+            if (isinstance(s, int) is not True):
+                raise TypeError
+            else:
+                self.__size = s
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")

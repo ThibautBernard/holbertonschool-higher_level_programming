19b22ff67ae15e85a29d64507f0ec7f16b22e5ef
Thibaut <thibautbernard@sfr.fr>
2021-01-05 14:38:11 +0000

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+Square class - initialize a square
+"""
+
+
 class Square:
     """ square class"""
     def __init__(self, square):
-        """ initialize class """
+        """
+        initialize the class and attributes class
+
+        Attributes: square(int): size of the square
+        """
         self.__size = square

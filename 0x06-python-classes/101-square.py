dbc709d6db16dffc1bf0b84d9e1959ba2e2bf266
Thibaut <thibautbernard@sfr.fr>
2021-01-05 15:31:35 +0000

    fix

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
+"""
+Square class - setter and initialize the class
+
+"""
+
+
 class Square:
+    """ square class """
     def __init__(self, value=0, position=(0, 0)):
+        """Initialize attributs
+        Args:
+            value (int): size of square
+            position (tuples): position
+        """
         self._size = value
         self._position = position
 
     def __str__(self):
+        """ invoke print method if print() use on the instance"""
         self.my_print()
         return ""
 
     @property
     def size(self):
+        """getter """
         return self._size
 
     @size.setter
     def size(self, value):
+        """setter"""
         try:
             if (value < 0):
                 raise ValueError
@@ -28,10 +43,12 @@ class Square:
 
     @property
     def position(self):
+        """getter"""
         return self._position
 
     @position.setter
     def position(self, value):
+        """setter"""
         try:
             for i in value:
                 if (isinstance(i, int) is not True):
@@ -41,9 +58,11 @@ class Square:
             print("position must be a tuple of 2 positive integers")
 
     def area(self):
+        """Return Area """
         return self._size * self._size
 
     def my_print(self):
+        """Print square"""
         counter = 0
         if (self._size == 0):
             print()

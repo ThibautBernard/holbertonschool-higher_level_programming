1440c2257102c696dcb929dc821f1b8a67c538a4
Thibaut <thibautbernard@sfr.fr>
2021-01-05 14:47:40 +0000

    fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
+"""
+Square class - setter and initialize the class
+
+"""
+
+
 class Square:
+    """
+        Square class
+    """
     def __init__(self, size=0):
+        """ Initialize
+        Args:
+            size (int): size of the square
+
+        """
         self.set_size(size)
 
     def set_size(self, s):
+        """
+            setter of the size
+        Args:
+            s (int): size passed to check
+        """
         try:
             if (s < 0):
                 raise ValueError
@@ -17,4 +36,7 @@ class Square:
             print("size must be >= 0")
 
     def area(self):
+        """
+            Return the area
+        """
         return self.__size * self.__size

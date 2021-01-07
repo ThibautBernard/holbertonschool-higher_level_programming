9d1c954050bd42527f4f73fe5fd80bcae1947e48
Thibaut <thibautbernard@sfr.fr>
2021-01-07 13:24:16 +0000

    feat()

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+say_my_name - print name and last name
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ function that print name and last name """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if not first_name:
+        raise TypeError("first_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))

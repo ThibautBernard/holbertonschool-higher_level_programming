f698500c6966880e906653a1834da79c7b0a222d
Thibaut <thibautbernard@sfr.fr>
2021-01-07 14:44:37 +0000

    feat()

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+print_square - print a square
+
+"""
+
+
+def print_square(size):
+    """ function that print a square from size """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size < 0 and isinstance(size, float):
+        raise TypeError("size must be an integer")
+    for i in range(0, size):
+        for y in range(0, size):
+            print("#", end="")
+        print()

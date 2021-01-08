4c38e9717a0472439f19d259b095dd6d404b8398
Thibaut <thibautbernard@sfr.fr>
2021-01-08 11:31:26 +0000

    fix(): update doc

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,7 +6,11 @@ add_integer - add two integer
 
 
 def add_integer(a, b=98):
-    """ function that add two integer """
+    """ function that add two integer
+    Args:
+        a: (int, float): first int
+        b: (int, float): second int
+    """
     inf = float('inf')
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")

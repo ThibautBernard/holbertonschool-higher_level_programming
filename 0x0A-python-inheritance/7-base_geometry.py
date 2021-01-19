f3fe5be56cae3c354094f6da9f4c09837791ebd3
Thibaut <thibautbernard@sfr.fr>
2021-01-19 13:11:34 +0000

    feat()

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class base geometry"""
+
+
+class BaseGeometry:
+    """class baseGeometry class that does something"""
+    def area(self):
+        """ raise an exception if called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ take a string and a value and we need to check what passed
+        Args:
+            name: (string): name of the string
+            value: (integer): must be only a integer
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0 or value == 0:
+            raise ValueError("{} must be greater than 0".format(name))

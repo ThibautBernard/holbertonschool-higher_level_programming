addfdf1900995fe5a2117c7cc3dca8c4377bb160
Thibaut <thibautbernard@sfr.fr>
2021-01-19 17:12:51 +0000

    fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""function that inherits from"""
+
+
 def inherits_from(obj, a_class):
     """check if the object is an instance of
     a class that inherited (directly or indirectly)

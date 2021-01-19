e16890d4b25d75c96840b7b0ffa933166c289703
Thibaut <thibautbernard@sfr.fr>
2021-01-19 17:26:53 +0000

    fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,6 +16,7 @@ class BaseGeometry:
         """
         if type(name) != str:
             name = "height"
+            raise TypeError("{} must be a string".format(name))
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value < 0 or value == 0:

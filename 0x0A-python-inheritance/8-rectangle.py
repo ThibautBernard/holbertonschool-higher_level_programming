4d5aa7589889f4199e30e83cce1128a77ec36252
Thibaut <thibautbernard@sfr.fr>
2021-01-19 14:46:14 +0000

    fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,5 +7,10 @@ class Rectangle(BaseGeometry):
     """class rectangle that inerith from BaseGeometry"""
     def __init__(self, width, height):
         """Init function that initialize"""
-        self.__width = self.integer_validator(width, height)
-        self.__height = self.integer_validator(width, height)
+        try:
+            super().integer_validator("width", width)
+            super().integer_validator("height", height)
+            self.__width = width
+            self.__height = height
+        except Exception as e:
+                print("[{}] {}".format(e.__class__.__name__, e))

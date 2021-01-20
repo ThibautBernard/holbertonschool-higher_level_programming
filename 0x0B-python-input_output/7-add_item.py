7ae00879e3cb5934af50c8cdb0f01c180f9a4934
Thibaut <thibautbernard@sfr.fr>
2021-01-20 10:48:40 +0000

    feat()

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import sys
+import os.path
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+if not os.path.isfile('add_item.json'):
+    l = []
+    save_to_json_file(l, "add_item.json")
 
 l = load_from_json_file("add_item.json")
 

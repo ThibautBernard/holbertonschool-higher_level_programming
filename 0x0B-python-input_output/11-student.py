36611849de8313f9e0a0b541db8c955f4ab79496
Thibaut <thibautbernard@sfr.fr>
2021-01-20 15:52:26 +0000

    feat()

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Class student"""
+
+
+class Student:
+    """Class of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionnary of all the attributes"""
+        if type(attrs) == list:
+            di = {}
+            dict_attr = self.__dict__
+            for key in attrs:
+                if not type(key) == str:
+                    return self.__dict__
+                for name in dict_attr:
+                    if key == name:
+                        di[key] = dict_attr[name]
+            return di
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ reload the object deserialisation"""
+        for key in json:
+            if key in self.__dict__:
+                setattr(self, key, json[key])

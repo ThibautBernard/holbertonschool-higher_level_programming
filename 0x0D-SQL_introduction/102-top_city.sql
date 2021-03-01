32703e71fd0d25a41f0c18390130f4bbe39dc94c
Thibaut <thibautbernard@sfr.fr>
2021-03-01 11:13:46 +0000

    fix

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,2 @@
 -- import a database and print average of temperature of groupe of city during july and august
-use hbtn_0c_0;
-source temperatures.sql;
 SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month >= 7 and month <= 8 GROUP BY city ORDER BY avg_tmp DESC LIMIT 3;

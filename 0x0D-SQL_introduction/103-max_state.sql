809af7f13543dfd22358900743b5a0588778cf26
Thibaut <thibautbernard@sfr.fr>
2021-03-01 11:22:52 +0000

    fix

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 -- import a database and print the max temperature of a state
 -- use hbtn_0c_0;
 -- source temperatures.sql;
-SELECT state, MAX(value) as avg_temp FROM temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY state;

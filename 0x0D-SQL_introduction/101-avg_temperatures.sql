2f87f573a7ff95a31b60db70310065ef756fd441
Thibaut <thibautbernard@sfr.fr>
2021-03-01 01:49:18 +0000

    feat()

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- import a database and print average of temperature of groupe of city
+use hbtn_0c_0;
+source temperatures.sql;
+SELECT city, AVG(value) as avg_tmp FROM temperatures GROUP BY city ORDER BY avg_tmp DESC;

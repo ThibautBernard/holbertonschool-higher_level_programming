57edb295ce6e38aed2d7e88628b0fdf203ff10f1
Thibaut <thibautbernard@sfr.fr>
2021-03-02 12:46:15 +0000

    fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- print all city from california
 -- blabla
-SELECT id, name FROM cities WHERE state_id=(SELECT id from states WHERE name="California") ORDER BY cities.id;
+SELECT id, name FROM cities WHERE state_id in (SELECT id from states WHERE name="California") ORDER BY cities.id;

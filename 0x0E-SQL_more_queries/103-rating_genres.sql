696e52efd8ae61e7d4b5eefd4197ff58e28a9447
Thibaut <thibautbernard@sfr.fr>
2021-03-03 11:05:14 +0000

    fix

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,3 +1,3 @@
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating
 
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating FROM tv_genres JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id JOIN tv_show_ratings ON tv_show_ratings.show_id=tv_show_genres.show_id GROUP BY tv_show_genres.genre_id ORDER BY rating DESC;
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating FROM tv_genres JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id JOIN tv_show_ratings ON tv_show_ratings.show_id=tv_show_genres.show_id GROUP BY tv_show_genres.genre_id ORDER BY rating DESC;

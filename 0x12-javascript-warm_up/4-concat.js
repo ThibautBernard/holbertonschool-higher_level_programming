043fbcf064124e938557784bbd570205cbeb79e9
ThibautBernard <thibautbernard@sfr.fr>
2021-03-22 10:00:38 +0000

    feat()

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const nbArgs = process.argv.length;
+if (nbArgs === 2) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+}

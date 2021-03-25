0251532fe03bd78a54c9affced07026c9e540f04
ThibautBernard <thibautbernard@sfr.fr>
2021-03-25 12:30:25 +0000

    feat()

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    ORM Mapper to tables States
+"""
+import sys
+import MySQLdb
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)

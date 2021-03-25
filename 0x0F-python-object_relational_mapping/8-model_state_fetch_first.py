e02101779a702248abd540f40e715180a02a9642
ThibautBernard <thibautbernard@sfr.fr>
2021-03-25 12:56:33 +0000

    fix

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,6 +10,5 @@ if __name__ == "__main__":
                            sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    our_user = session.query(State).all()
-    for i in our_user:
-        print("{:d}: {}".format(i.id, i.name))
+    state = session.query(State).first()
+    print("{:d}: {}".format(state.id, state.name))

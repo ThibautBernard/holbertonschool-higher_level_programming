a514b87f6ccf1531c31627f40ba777cc8003c01f
ThibautBernard <thibautbernard@sfr.fr>
2021-03-25 10:20:08 +0000

    feat()

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    u_name = sys.argv[1]
+    p = sys.argv[2]
+    db_name = sys.argv[3]
+    h = 'localhost'
+    search_n = sys.argv[4]
+    db = MySQLdb.connect(host=h, port=3306, user=u_name, passwd=p, db=db_name)
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name= %s"
+    numrows = cur.execute(sql, (search_n, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)

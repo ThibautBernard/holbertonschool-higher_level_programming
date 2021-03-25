b9787cf5a7c6e24b703e030932aa5f31715bca28
ThibautBernard <thibautbernard@sfr.fr>
2021-03-25 09:45:12 +0000

    feat()

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    u_name = sys.argv[1]
+    p = sys.argv[2]
+    db_name = sys.argv[3]
+    h = 'localhost'
+    db = MySQLdb.connect(host=h, port=3306, user=u_name, passwd=p, db=db_name)
+    cur = db.cursor()
+    numrows = cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)

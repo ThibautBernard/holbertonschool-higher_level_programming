693c44ddb4308b18a2fdd293980447269cde8220
ThibautBernard <thibautbernard@sfr.fr>
2021-03-25 09:58:30 +0000

    feat()

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    u_name = sys.argv[1]
+    p = sys.argv[2]
+    db_name = sys.argv[3]
+    h = 'localhost'
+    search_n = sys.argv[4]
+    db = MySQLdb.connect(host=h, port=3306, user=u_name, passwd=p, db=db_name)
+    cur = db.cursor()
+    numrows = cur.execute("SELECT * FROM states WHERE name='%s'" % search_n)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)

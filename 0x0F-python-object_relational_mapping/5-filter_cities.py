5ab1519f7c128c59475dff6dfb4ca3b40cc51314
ThibautBernard <thibautbernard@sfr.fr>
2021-03-25 11:15:40 +0000

    feat(à

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    u_name = sys.argv[1]
+    p = sys.argv[2]
+    db_name = sys.argv[3]
+    state_s = sys.argv[4]
+    h = 'localhost'
+    db = MySQLdb.connect(host=h, port=3306, user=u_name, passwd=p, db=db_name)
+    cur = db.cursor()
+    sql = "SELECT cities.name FROM cities JOIN \
+          states ON cities.state_id=states.id WHERE states.name=%s"
+    numrows = cur.execute(sql, (state_s, ))
+    rows = cur.fetchall()
+    for i, row in enumerate(rows):
+        for x in row:
+            if i + 1 < len(rows):
+                print(x, end=", ")
+            else:
+                print(x, end="")
+    print()

f2ee1edeef54215acbdbc2858b1c40e482a8438a
ThibautBernard <thibautbernard@sfr.fr>
2021-03-25 11:00:52 +0000

    feat()

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    u_name = sys.argv[1]
+    p = sys.argv[2]
+    db_name = sys.argv[3]
+    h = 'localhost'
+    db = MySQLdb.connect(host=h, port=3306, user=u_name, passwd=p, db=db_name)
+    cur = db.cursor()
+    sql = "SELECT cities.id, cities.name, states.name \
+          FROM cities JOIN states WHERE cities.state_id=states.id"
+    numrows = cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)

e4b1b1ba2510b30e8858104f4deae630f8972647
ThibautBernard <thibautbernard@sfr.fr>
2021-03-27 18:09:31 +0000

    feat()

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """map a state"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)

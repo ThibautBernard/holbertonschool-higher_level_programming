0c03c0ef59564003daf722256e9c2b73ee219cb9
ThibautBernard <thibautbernard@sfr.fr>
2021-04-08 10:33:22 +0000

    fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # display body content and send a post request with variables in url
-curl -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"

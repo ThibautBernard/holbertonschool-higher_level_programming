d0de75b7de31bfed272b8870b4022f8f6946c43c
ThibautBernard <thibautbernard@sfr.fr>
2021-04-08 10:10:05 +0000

    fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # display allow http method avalaible
-curl -sL -w "%{allow}" -I -o /dev/null "$1"
-# curl -sI -L -X "$1" | grep Allow | cut -d: -f2
+ curl -sI -L "$1" | grep Allow | cut -d: -f2

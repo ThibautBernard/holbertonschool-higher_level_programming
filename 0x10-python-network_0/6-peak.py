5791ae806d9275957f07cb56524dc833f81cd6dc
ThibautBernard <thibautbernard@sfr.fr>
2021-04-08 13:05:05 +0000

    fix: use divide and conquer method

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,27 @@
 
 
 def find_peak(list_of_integers):
-    """ sort the array and get the last """
+    """
+        Divide and conquer method
+        split the array and subarray in two at each recursion
+        return the peak if the peak is greather than his
+        neighbors
+    """
     if not list_of_integers:
         return None
-    list_of_integers.sort()
-    return list_of_integers[-1]
+    l = len(list_of_integers)
+    if l == 1:
+        return list_of_integers[0]
+    if l == 2:
+        if list_of_integers[1] > list_of_integers[0]:
+            return list_of_integers[1]
+        else:
+            return list_of_integers[0]
+    mid = round(l / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid:])

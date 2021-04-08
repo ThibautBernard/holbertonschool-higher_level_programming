3fc716c9a203034a23a56868546ca4bfb74f23f6
ThibautBernard <thibautbernard@sfr.fr>
2021-04-08 10:02:51 +0000

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # display body content if 200 status code
-status_code=$(curl -sL -w "%{http_code}" -I "$1" -o /dev/null)
-if [[ "$status_code" -eq 200 ]]
-then
-    curl -Ls "$1"
-fi
+curl -sL "$1"

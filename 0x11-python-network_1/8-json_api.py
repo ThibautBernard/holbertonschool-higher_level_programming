cb6fa0867b60170cf2108d8fa7d2f9393ee55802
ThibautBernard <thibautbernard@sfr.fr>
2021-04-09 12:16:03 +0000

    fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,7 @@
 import requests
 import sys
 if __name__ == "__main__":
-    if sys.argv[1]:
+    if len(sys.argv) == 2 and sys.argv[1]:
         d = {'q': sys.argv[1]}
     else:
         d = {'q': ''}

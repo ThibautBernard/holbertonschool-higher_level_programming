ecc7de1095a7c776b9d716948565100d354c8579
ThibautBernard <thibautbernard@sfr.fr>
2021-04-09 11:27:05 +0000

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,10 +3,10 @@
    send a post request
 """
 from urllib import request, parse
-
 import sys
-data = parse.urlencode({"email": sys.argv[2]})
-data = data.encode('ascii')
-with request.urlopen(sys.argv[1], data) as response:
-    html = response.read()
-print(html.decode("utf8"))
+if __name__ == "__main__":
+    data = parse.urlencode({"email": sys.argv[2]})
+    data = data.encode('ascii')
+    with request.urlopen(sys.argv[1], data) as response:
+        html = response.read()
+    print(html.decode("utf8"))

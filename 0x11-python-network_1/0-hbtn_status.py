6281cefca90250f48039afbf91da0cdea8242dd7
ThibautBernard <thibautbernard@sfr.fr>
2021-04-09 11:23:32 +0000

    fix:

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@
     request url and print his value, type, utf8 content
 """
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-print("Body response:")
-print("    - type: {}".format(type(html)))
-print("    - content: {}".format(html))
-print("    - utf8 content: {}".format(html.decode('utf8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+    print("Body response:")
+    print("    - type: {}".format(type(html)))
+    print("    - content: {}".format(html))
+    print("    - utf8 content: {}".format(html.decode('utf8')))

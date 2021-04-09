ae6f21ce3fe7e5a3cd54f18df289bfbcf8388f78
ThibautBernard <thibautbernard@sfr.fr>
2021-04-09 12:56:51 +0000

    fix: add auth for the checker

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         h = {'Accept': 'application/vnd.github.v3+json'}
         u = "https://api.github.com/users/{}".format(sys.argv[1])
-        r = requests.get(u, headers=h)
+        r = requests.get(u, auth=(sys.argv[1], sys.argv[2]), headers=h)
         if 'id' in r.json():
             print(r.json()['id'])
         else:

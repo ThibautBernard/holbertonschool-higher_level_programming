b30d0babdb403b8b89759015f2c660de4deb2561
ThibautBernard <thibautbernard@sfr.fr>
2021-04-09 11:37:47 +0000

    fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,4 +6,5 @@ import requests
 import sys
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+    if 'X-Request-Id' in r.headers and r:
+        print(r.headers['X-Request-Id'])

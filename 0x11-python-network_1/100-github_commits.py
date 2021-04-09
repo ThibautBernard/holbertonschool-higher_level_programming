f6bca4b356d83b59903261aa2b40f9fe04f58bfd
ThibautBernard <thibautbernard@sfr.fr>
2021-04-09 13:55:00 +0000

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,11 +8,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         h = {'Accept': 'application/vnd.github.v3+json'}
         u = "https://api.github.com/repos/{}/{}/commits"\
-            .format(sys.argv[1], sys.argv[2])
+            .format(sys.argv[2], sys.argv[1])
         r = requests.get(u, params={'per_page': 10}, headers=h)
         for i, k in enumerate(r.json()):
             s = r.json()[i]['sha']
             n = r.json()[i]['commit']['author']['name']
             print("{}: {}".format(s, n))
-    else:
-        print('None')

8e191087ca5162dee562f9efcd0079b542ea2044
ThibautBernard <thibautbernard@sfr.fr>
2021-05-12 09:56:05 +0000

    feat(): task 11

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+/**
+  change font color of header element when the dom is 'ready'
+**/
+document.addEventListener('DOMContentLoaded', (evt) => {
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});

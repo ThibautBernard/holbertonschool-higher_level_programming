ebb9bf8cb8a981ac66b4b2a827d77ab5d4ce1250
ThibautBernard <thibautbernard@sfr.fr>
2021-05-12 08:13:13 +0000

    feat: second script

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,8 @@
+/**
+    Select the header and change the font color with JQUERY
+**/
+const $ = window.$;
+$(document).ready(function () {
+  const header = $('header');
+  header.css('color', '#FF0000');
+});
